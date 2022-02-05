83844104dbbe27685a4f209c9f307b4542cb3944
hoandt <hdt1302@gmail.com>
2022-02-05 15:47:46 +0700

    update ping url

--- pages/ws.js
+++ pages/ws.js
@@ -7,7 +7,10 @@ function WSPage() {
     const kline = { id: 1, amt: 2000 };
     const getData = async () => {
       try {
-        const response = await api.post("/post.php", kline);
+        const response = await axios.post(
+          "https://vnconsumer.com/hoan/crypto/api/post.php",
+          kline
+        );
         setPostStatus(response.data);
       } catch (error) {
         console.log("[pages/ws.js]: ", error);
